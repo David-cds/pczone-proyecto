c84ff7bb750733d2af48a9b90575885d6d373fec
David-cds <186015731+David-cds@users.noreply.github.com>
2026-05-28 21:39:05 +0200

    solucion conflictos

--- admin/crear_producto.php
+++ admin/crear_producto.php
@@ -16,29 +16,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     move_uploaded_file($_FILES["imagen"]["tmp_name"], $ruta);
 
-<<<<<<< Updated upstream
     // INSERTAR EN BD
     $sql = "INSERT INTO productos (nombre, descripcion, precio, categoria, imagen) 
             VALUES (:nombre, :descripcion, :precio, :categoria, :imagen)";
-=======
-   
-    $sql = "INSERT INTO productos (nombre, descripcion, precio, categoria_id,stock,imagen) 
-            VALUES (:nombre, :descripcion, :precio, :categoria_id,:stock,:imagen)";
->>>>>>> Stashed changes
 
     $query = $conexion->prepare($sql);
 
     $query->bindParam(":nombre", $nombre);
     $query->bindParam(":descripcion", $descripcion);
     $query->bindParam(":precio", $precio);
-<<<<<<< Updated upstream
     $query->bindParam(":categoria", $categoria);
     $query->bindParam(":imagen", $imagen);
-=======
-    $query->bindParam(":categoria_id", $categoria_id);
-    $query->bindParam(":stock", $stock);
-    $query->bindParam(":imagen", $ruta);
->>>>>>> Stashed changes
 
     $query->execute();
 
@@ -46,25 +34,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-<<<<<<< Updated upstream
 // FORMULARIO 
-=======
-<main>
-
-    <h2 class="admin-title">Añadir Nuevo Producto</h2>
->>>>>>> Stashed changes
 
     <form action="crear_producto.php" method="POST" enctype="multipart/form-data">
         
         <label for="nombre">Nombre del Producto:</label>
         <input type="text" id="nombre" name="nombre" placeholder="Ej. Tarjeta Gráfica RTX 4060" required>
 
-<<<<<<< Updated upstream
 <form method="POST" enctype="multipart/form-data">
-=======
-        <label for="precio">Precio (€):</label>
-        <input type="number" id="precio" name="precio" step="0.01" placeholder="Ej. 299.99" required>
->>>>>>> Stashed changes
 
         <label for="descripcion">Descripción:</label>
         <textarea id="descripcion" name="descripcion" required></textarea>
@@ -74,7 +51,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         <option value="">Selecciona categoría</option>
 
-<<<<<<< Updated upstream
     <label>Categoría:</label><br>
     <select name="categoria">
         <option value="GPU">GPU</option>
@@ -87,34 +63,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <label>Imagen:</label><br>
     <input type="file" name="imagen" required><br><br>
-=======
-        <?php foreach($categorias as $categoria){ ?>
-
-            <option value="<?= $categoria['id'] ?>">
-                <?= $categoria['nombre'] ?>
-            </option>
-
-        <?php } ?>
-
-        </select>
-
-        <label for="stock">Stock Disponible:</label>
-        <input type="number" id="stock" name="stock" placeholder="Ej. 15" required>
->>>>>>> Stashed changes
 
         <label for="imagen">Imagen del Producto:</label>
         <input type="file" id="imagen" name="imagen" accept="image/*" required>
 
-<<<<<<< Updated upstream
 </form>
-=======
-        <button type="submit">Guardar Producto</button>
-        
-        <a href="index.php" class="btn-add" style="margin-top: 15px !important;">Volver al Panel</a>
-    </form>
-
-</main>
-
-<?php include_once("../includes/footer.php"); ?>
-
->>>>>>> Stashed changes
--- admin/editar_producto.php
+++ admin/editar_producto.php
@@ -1,14 +1,6 @@
 <?php 
 include("../includes/auth.php"); 
 include("../includes/conexion.php");
-<<<<<<< Updated upstream
-=======
-include("../includes/header.php");
-
-// Categorías
-$queryCat = $conexion->query("SELECT * FROM categorias");
-$categorias = $queryCat->fetchAll(PDO::FETCH_ASSOC);
->>>>>>> Stashed changes
 
 // Obtener ID por GET
 if(!isset($_GET["id"])){
@@ -38,39 +30,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $nombre = $_POST["nombre"];
     $descripcion = $_POST["descripcion"];
     $precio = $_POST["precio"];
-<<<<<<< Updated upstream
     $categoria = $_POST["categoria"];
 
     $sql = "UPDATE productos 
             SET nombre = :nombre, precio = :precio, categoria = :categoria 
             WHERE id = :id";
-=======
-    $categoria_id = $_POST["categoria_id"];
-    $stock = $_POST["stock"];
-
-    $rutaImagen = $producto['imagen'];
-
-if(!empty($_FILES["imagen"]["name"])){
-
-    $imagen = $_FILES["imagen"]["name"];
-
-    $rutaImagen = "img/productos/" . $imagen;
-
-    move_uploaded_file(
-        $_FILES["imagen"]["tmp_name"],
-        "../" . $rutaImagen
-    );
-}
-
-   $sql = "UPDATE productos 
-        SET nombre = :nombre,
-        descripcion = :descripcion,
-        precio = :precio,
-        categoria_id = :categoria_id,
-        stock = :stock,
-        imagen = :imagen
-        WHERE id = :id";
->>>>>>> Stashed changes
 
     $update = $conexion->prepare($sql);
 
@@ -99,7 +63,6 @@ if(!empty($_FILES["imagen"]["name"])){
         <label for="nombre">Nombre:</label>
         <input type="text" id="nombre" name="nombre" value="<?= htmlspecialchars($producto['nombre']) ?>" required>
 
-<<<<<<< Updated upstream
     <label>Categoría:</label><br>
     <select name="categoria">
         <option value="GPU" <?= $producto['categoria']=="GPU" ? "selected" : "" ?>>GPU</option>
@@ -109,10 +72,6 @@ if(!empty($_FILES["imagen"]["name"])){
         <option value="Raton" <?= $producto['categoria']=="Raton" ? "selected" : "" ?>>Ratón</option>
         <option value="Disco" <?= $producto['categoria']=="Disco" ? "selected" : "" ?>>Disco</option>
     </select><br><br>
-=======
-        <label for="descripcion">Descripción:</label>
-        <textarea id="descripcion" name="descripcion" required><?= htmlspecialchars($producto['descripcion']) ?></textarea>
->>>>>>> Stashed changes
 
         <label for="precio">Precio (€):</label>
         <input type="number" id="precio" name="precio" step="0.01" value="<?= $producto['precio'] ?>" required>
@@ -120,31 +79,5 @@ if(!empty($_FILES["imagen"]["name"])){
         <label for="stock">Stock:</label>
         <input type="number" id="stock" name="stock" value="<?= $producto['stock'] ?>"required>
 
-<<<<<<< Updated upstream
 <br>
 <a href="index.php">⬅ Volver</a>
-=======
-        <label for="categoria_id">Categoría:</label>
-        <select id="categoria_id" name="categoria_id">
-           <?php foreach($categorias as $cat){ ?>
-                <option value="<?= $cat['id'] ?>" 
-                    <?= $producto['categoria_id'] == $cat['id'] ? "selected" : "" ?>>
-                    <?= htmlspecialchars($cat['nombre']) ?>
-                </option>
-            <?php } ?>
-        </select>
-
-        <label>Imagen actual:</label>
-        <img src="../<?= $producto['imagen'] ?>" width="120">
-        <label for="imagen">Nueva imagen:</label>
-        <input type="file" name="imagen">
-
-        <button type="submit">Guardar cambios</button>
-        
-        <a href="index.php" class="btn-add" style="margin-top: 15px !important;">⬅ Volver al Panel</a>
-    </form>
-
-</main>
-
-<?php include_once("../includes/footer.php"); ?>
->>>>>>> Stashed changes
