9b7b6bca07c14856f530035b07c7b48b4dc46e19
David-cds <186015731+David-cds@users.noreply.github.com>
2026-05-29 12:32:38 +0200

    Update agregar_carrito.php

--- agregar_carrito.php
+++ agregar_carrito.php
@@ -1,6 +1,28 @@
 <?php
 
+include("includes/conexion.php");
+
 $id = $_GET["id"];
+
+// COMPROBAR STOCK
+$sql = "SELECT stock FROM productos WHERE id = :id";
+
+$query = $conexion->prepare($sql);
+
+
+$query->bindParam(":id", $id);
+
+$query->execute();
+
+$producto = $query->fetch(PDO::FETCH_ASSOC);
+
+// SI NO HAY STOCK
+if($producto['stock'] <= 0){
+
+    echo "Producto sin stock";
+    exit();
+
+}
 
 if(isset($_COOKIE["carrito"])){
 
@@ -17,6 +39,18 @@ $carrito .= $id . ",";
 // guardar cookie
 setcookie("carrito", $carrito, time()+3600);
 
+// RESTAR STOCK
+$sqlStock = "UPDATE productos
+             SET stock = stock - 1
+             WHERE id = :id";
+
+$queryStock = $conexion->prepare($sqlStock);
+
+$queryStock->bindParam(":id", $id);
+
+$queryStock->execute();
+
+// VOLVER
 header("Location: index.php");
 
 ?>
